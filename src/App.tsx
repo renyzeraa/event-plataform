2f35c711432bd981ccf11a9bd0f069ca53ee9ac6
Renan Silva <renansilvaytB@gmail.com>
2025-11-03 10:27:08 -0300

    feat: app.tsx

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { ApolloProvider } from "@apollo/client/react";
+import { Router } from "./router";
+import { client } from "./lib/apollo";
+import { BrowserRouter } from "react-router-dom";
+
+export function App() {
+    return (
+        <ApolloProvider client={client}>
+            <BrowserRouter>
+                <Router />
+            </BrowserRouter>
+        </ApolloProvider>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-//@ts-expect-error not error
-import '../src/styles/global.css'
-import { App } from './app.tsx'
+// @ts-expect-error because of css module
+import './styles/global.css'
+import { App } from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
